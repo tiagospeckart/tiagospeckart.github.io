ce3d655b67687c50f0c4999468ebfac2540fecdf
Tiago Martins Speckart <75458110+tiagospeckart@users.noreply.github.com>
2023-07-12 21:34:59 -0300

    feat: bandeiras

--- paises/scripts/script.js
+++ paises/scripts/script.js
@@ -11,6 +11,8 @@ $(document).ready(function(){
                     output += `<p>Capital: ${countryData.capital[0]}</p>`;
                     output += `<p>Área: ${countryData.area} km²</p>`;
                     output += `<p>População: ${countryData.population}</p>`;
+                    var flagUrl = `https://flagcdn.com/${countryData.cca2.toLowerCase()}.svg`;
+                    output += `<img src="${flagUrl}" alt="Flag of ${countryData.name.common}"/>`;
                     $('#countryInfo').html(output);
 
                     var googlePhotosLink = `https://www.google.com/search?q=${country}&tbm=isch`;
